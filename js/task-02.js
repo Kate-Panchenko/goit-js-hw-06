54617f60210171efb9f5b40de19c8f4f578158b3
Kate Panchenko <k.panchenko12345@gmail.com>
2022-12-06 11:20:19 +0200

    Task-02 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+  
+  
+for (const ingredient of ingredients) {
+  const ingredientEl = document.createElement("li");
+  ingredientEl.textContent = ingredient;
+  console.log(ingredientEl);
+
+  ingredientEl.classList.add("item");
+
+  const ingredientsEl = document.querySelector("ul#ingredients");
+  ingredientsEl.append(ingredientEl);
+}
+
+
