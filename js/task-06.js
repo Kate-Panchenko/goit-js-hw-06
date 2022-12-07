dc3ed84a2a4c701a828569a90c52b6cf16c1982b
Kate Panchenko <k.panchenko12345@gmail.com>
2022-12-07 16:50:07 +0200

    Update task-07.js

--- js/task-06.js
+++ js/task-06.js
@@ -4,6 +4,7 @@ textInput.addEventListener("blur", event => {
     if (event.target.value.length == textInput.getAttribute('data-length')) {
         textInput.classList.add('valid');
         textInput.classList.remove('invalid');
+
     } else {
         textInput.classList.add('invalid');
         textInput.classList.remove('valid');
