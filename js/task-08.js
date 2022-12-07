f208c0b2e8493a5fcc27acf66482f1279db4c27f
Kate Panchenko <k.panchenko12345@gmail.com>
2022-12-07 17:04:18 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('form.login-form');
 
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const { elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Всі поля мають бути заповнені!")
+    } 
+
+    const fullForm = { email: email.value, password: password.value };
+    console.log(fullForm);
+    event.currentTarget.reset();
+};
